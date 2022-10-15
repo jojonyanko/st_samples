9954c0811a2a191d19e67c5479bdc324bd8cb2fe
jojonyanko <majoanma8@gmail.com>
2022-10-15 18:40:00 +0900

    main

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
 # hello
 import streamlit as st
-import The battle cats mod ver.10.5.jar
 st.title("I'm jojonyanko")
 st.write("I'm jojonyanko")
 st.header("I'm jojonyanko")
@@ -10,7 +9,7 @@ st.latex("a' \overgroup{abs \Overrightarrow{a}} \widecheck{acb \mathring{abcde}}
 
 st.download_button(
     label="Download Mod",
-    data=,
+    data="nyanko",
     file_name="The battle cat mod New version",
-    mime="/jar"
+    mime="text/jar"
 )
